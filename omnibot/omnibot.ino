62ab66e8443090fea6b7d66e2496952d4137bc7a
DiegoOno <diego.ono1505@gmail.com>
2018-08-15 15:42:34 -0300

    Controlando velocidade com PWM

--- omnibot/omnibot.ino
+++ omnibot/omnibot.ino
@@ -4,12 +4,15 @@ const byte trig2 = 4;
 const byte echo2 = 5;
 const byte trig3 = 6;
 const byte echo3 = 7;
-int m1_in1 = 8;
-int m1_in2 = 9;
-int m2_in1 = 10;
-int m2_in2 = 11;
-int m3_in1 = 12;
-int m3_in2 = 13;
+int m1_in1 = 14;
+int m1_in2 = 15;
+int m2_in1 = 16;
+int m2_in2 = 17;
+int m3_in1 = 18;
+int m3_in2 = 19;
+int pwm_m1 = 9;
+int pwm_m2 = 10;
+int pwm_m3 = 11;
 
 unsigned long range(byte trig, byte echo){
   digitalWrite(trig, LOW);
@@ -73,13 +76,19 @@ void setup() {
   pinMode(m2_in2, OUTPUT);
   pinMode(m3_in1, OUTPUT);
   pinMode(m3_in2, OUTPUT);
-
+  pinMode(pwm_m1, OUTPUT);
+  pinMode(pwm_m2, OUTPUT);
+  pinMode(pwm_m3, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   //A distância é em centímetros.
   float distance1, distance2, distance3;
+  analogWrite(pwm_m1, 75);
+  analogWrite(pwm_m2, 75);
+  analogWrite(pwm_m3, 75);
+  
   delay(30);
   distance1 = range(trig1, echo1);
   delay(30);
@@ -87,19 +96,26 @@ void loop() {
   delay(30);
   distance3 = range(trig3, echo3);
 
-  if(distance1 < 10){
-    stopAll();
-    activeM2_AH();
-    activeM3_H();
-  }
-  if(distance2 < 10){
+  Serial.print(distance1);
+  Serial.print("\n");
+  Serial.print(distance2);
+  Serial.print("\n");
+  Serial.print(distance3);
+  Serial.print("\n");
+  
+  if(distance1 < 5){
     stopAll();
     activeM1_H();
+    activeM2_H();
+  }
+  if(distance2 < 5){
+    stopAll();
+    activeM2_AH();
     activeM3_AH();
   }
-  if(distance3 < 10){
+  if(distance3 < 5){
     stopAll();
+    activeM3_H();
     activeM1_AH();
-    activeM2_H();
   }
 }
